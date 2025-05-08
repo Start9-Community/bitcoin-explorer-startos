881a80bdcd0a4a2ae48cfb7ab4d9751b77195382
aldum <aldum@users.noreply.github.com>
2025-05-08 17:26:56 +0200

    Initialize update/040

---
+++ startos/main.ts
@@ -0,0 +1,48 @@
+import { sdk } from './sdk'
+import { T } from '@start9labs/start-sdk'
+import { uiPort } from './utils'
+
+export const main = sdk.setupMain(async ({ effects, started }) => {
+  /**
+   * ======================== Setup (optional) ========================
+   *
+   * In this section, we fetch any resources or run any desired preliminary commands.
+   */
+  console.info('Starting Hello World!')
+
+  /**
+   * ======================== Additional Health Checks (optional) ========================
+   *
+   * In this section, we define *additional* health checks beyond those included with each daemon (below).
+   */
+  const additionalChecks: T.HealthCheck[] = []
+
+  /**
+   * ======================== Daemons ========================
+   *
+   * In this section, we create one or more daemons that define the service runtime.
+   *
+   * Each daemon defines its own health check, which can optionally be exposed to the user.
+   */
+  return sdk.Daemons.of(effects, started, additionalChecks).addDaemon(
+    'primary',
+    {
+      subcontainer: await sdk.SubContainer.of(
+        effects,
+        { imageId: 'hello-world' },
+        sdk.Mounts.of().addVolume('main', null, '/data', false),
+        'hello-world-sub',
+      ),
+      command: ['hello-world'],
+      ready: {
+        display: 'Web Interface',
+        fn: () =>
+          sdk.healthCheck.checkPortListening(effects, uiPort, {
+            successMessage: 'The web interface is ready',
+            errorMessage: 'The web interface is not ready',
+          }),
+      },
+      requires: [],
+    },
+  )
+})
