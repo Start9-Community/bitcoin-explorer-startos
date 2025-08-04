a8eacf358522cefb4cba96b81764f3f216e4b153
aldum <aldum@artixlinux.org>
2025-08-04 18:52:00 +0200

    chore: update bitcoind dep

--- startos/manifest.ts
+++ startos/manifest.ts
@@ -41,7 +41,7 @@ export const manifest = setupManifest({
       description: "Communicate with the Bitcoin Network",
       optional: false,
       s9pk:
-        "https://github.com/Start9Labs/bitcoind-startos/releases/download/v28.1.0.3-alpha.6/bitcoind.s9pk",
+        "https://github.com/Start9Labs/bitcoind-startos/releases/download/v28.1.0.3-alpha.8/bitcoind.s9pk",
     },
   },
 })
